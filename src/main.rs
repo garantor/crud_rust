917bf9c40005ada7cf19503ff726c841d994343e
asunday <asunday@innovative-technology.com>
2025-02-20 14:43:50 +0000

    added new endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 // use std::io;
 
-fn main() {
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Server is live and working fine!")
+}
+
+
+
+#[post("/register")]
+async fn register_user() -> impl Responder {
+    HttpResponse::Ok().body("hey Register")
+}
+
+
+#[get("/greet/{id}")]
+async fn great(id:web::Path<u32>) -> impl Responder {
+    format!("Hello woolred {} ", id)  
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
     println!("Hello, world!");
+    let port = 8080;
     // let mut user_input = String:: new();
     // io::stdin().read_line(&mut user_input).expect("failed to readlines ..... ");
     // println!("{} the user input ", user_input);
@@ -11,13 +36,16 @@ fn main() {
     // println!("{} the input ", user_input_1);
 
 
-    let number = {
-        let c =3;
-        c + 5
-    };
+    // let number = {
+    //     let c =3;
+    //     c + 5
+    // };
+
+    println!("Starting server now on {} port .... ", port);
+
+    HttpServer:: new(|| App:: new().service(register_user) .service(home).service(great)).bind(("127.0.0.1", port))?.workers(2).run().await
 
     
-    println!("{} the input ", number);
 
 
 }
