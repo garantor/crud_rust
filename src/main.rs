5624ee4909d3d4602912d5f89c382932101d0f68
asunday <asunday@innovative-technology.com>
2025-02-20 13:10:43 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+// use std::io;
+
+fn main() {
+    println!("Hello, world!");
+    // let mut user_input = String:: new();
+    // io::stdin().read_line(&mut user_input).expect("failed to readlines ..... ");
+    // println!("{} the user input ", user_input);
+
+    // let user_input_1:i64 = user_input.trim().parse().unwrap();
+
+    // println!("{} the input ", user_input_1);
+
+
+    let number = {
+        let c =3;
+        c + 5
+    };
+
+    
+    println!("{} the input ", number);
+
+
+}
